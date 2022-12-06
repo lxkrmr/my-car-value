d4e0e293529a229b58d8b54644ffed8f2d3158b2
Alex <lxkrmr@icloud.com>
2022-12-06 11:08:28 +0100

    feat: Store user id in cookie session

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['asdfdfd'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // ensures that additional fields are ignored, only specified fields will be considered
