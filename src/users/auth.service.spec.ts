3475ed4515ca1c382733aa486f9209d32543506a
Alex <lxkrmr@icloud.com>
2022-12-07 14:32:22 +0100

    test: create user with salted and hashed password

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,24 +3,48 @@ import { AuthService } from './auth.service';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
 
-it('can create an instance of auth service', async () => {
-  // create a fake copy of the users service
-  const fakeUserService: Partial<UsersService> = {
-    find: (email: string) => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password } as User),
-  };
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakeUserService,
-      },
-    ],
-  }).compile();
+describe('AuthService', () => {
+  let service: AuthService;
 
-  const service = module.get(AuthService);
+  beforeEach(async () => {
+    // create a fake copy of the users service
+    const fakeUserService: Partial<UsersService> = {
+      find: (email: string) => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUserService,
+        },
+      ],
+    }).compile();
 
-  expect(service).toBeDefined();
+    service = module.get(AuthService);
+  });
+
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
+
+  it('creates a new user with a salted and hashed password', async () => {
+    // given
+    const email = '<email>';
+    const password = '<password>';
+
+    // when
+    const result = await service.signup(email, password);
+
+    // then
+    const saltedAndHashedPassword = result.password;
+    const [salt, hash] = saltedAndHashedPassword.split('.');
+    expect(saltedAndHashedPassword).not.toStrictEqual(password);
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+    // and
+    expect(result.email).toStrictEqual(email);
+  });
 });
