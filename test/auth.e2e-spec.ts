df0df3b1d05c0e7b57db211d59321aad04abbc2f
Alex <lxkrmr@icloud.com>
2022-12-12 15:27:50 +0100

    test: Add test using the cookie

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -27,4 +27,23 @@ describe('Authentication system (e2e)', () => {
         expect(email).toEqual(email);
       });
   });
+
+  it('singup as a new user then get the currently loged in user', async () => {
+    // given
+    const email = 'any@email.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: '<password>' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });
